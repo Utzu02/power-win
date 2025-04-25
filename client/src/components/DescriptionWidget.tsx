464c5d9f19d806e26a20b9836c9aaf50ca62ed2a
Utzu02 <107960417+Utzu02@users.noreply.github.com>
2025-04-26 02:18:19 +0300

    scrollbar

--- client/src/components/DescriptionWidget.tsx
+++ client/src/components/DescriptionWidget.tsx
@@ -10,26 +10,28 @@ const DescriptionWidget = ({ syncedHeight }: DescriptionWidgetProps) => {
       className="p-[1px] rounded-[10px] bg-[linear-gradient(to_bottom,rgb(120,120,120),rgba(255,255,255,0.15))] w-fit m-0 mx-auto self-start"
       style={{ height: syncedHeight || 'auto' }}
     >
-      <div className="p-6 w-[45vw] h-full overflow-y-auto custom-scrollbar bg-gradient-to-b from-[#141414] to-[#1b1b1b] rounded-xl text-white border border-white/20">
-        <h2 className="text-2xl font-light mb-4">Descriere</h2>
-        <p className="mb-6 font-light text-xl">
-          Model: <strong>Porsche 911 (992) Turbo S</strong> | Sonderwunsch | Motor 3.8 | 650 CP
-        </p>
-        <div className="space-y-1 font-light leading-relaxed text-md">
-          <p>"Sport" buton</p>
-          <p>– Adaptiv scaune Sport incl. reglaj scaun 18-cai si pachet</p>
-          <p>– Memory</p>
-          <p>– Airbag cortina fata (Thoraxbag)</p>
-          <p>– Airbag pe partea pilotului / copilotului</p>
-          <p>– Airbag-uri cortina Porsche Side Impact Protection System</p>
-          <p>– (POSIP)</p>
-          <p>– Blocaj anti-pornire</p>
-          <p>– Carlig haine spatar scaun piele</p>
-          <p>– Caroserie: 2-usi</p>
-          <p>– Connect Plus (Bluetooth, Apple CarPlay, WLAN, VehicleTracking-System – sistem localizare vehicul)</p>
-          <p>– Cuplare automata pentru lumina de drum</p>
-          <p>– Cutie viteze 8 viteze – cutie viteze cu ambreiaj dublu</p>
-          <p>– (PDK)</p>
+      <div className="p-6 w-[45vw] h-full bg-gradient-to-b from-[#141414] to-[#1b1b1b] rounded-xl text-white border border-white/20">
+        <div className='overflow-y-auto h-full custom-scrollbar'>
+          <h2 className="text-2xl font-light mb-4">Descriere</h2>
+          <p className="mb-6 font-light text-xl">
+            Model: <strong>Porsche 911 (992) Turbo S</strong> | Sonderwunsch | Motor 3.8 | 650 CP
+          </p>
+          <div className="space-y-1 font-light leading-relaxed text-md">
+            <p>"Sport" buton</p>
+            <p>– Adaptiv scaune Sport incl. reglaj scaun 18-cai si pachet</p>
+            <p>– Memory</p>
+            <p>– Airbag cortina fata (Thoraxbag)</p>
+            <p>– Airbag pe partea pilotului / copilotului</p>
+            <p>– Airbag-uri cortina Porsche Side Impact Protection System</p>
+            <p>– (POSIP)</p>
+            <p>– Blocaj anti-pornire</p>
+            <p>– Carlig haine spatar scaun piele</p>
+            <p>– Caroserie: 2-usi</p>
+            <p>– Connect Plus (Bluetooth, Apple CarPlay, WLAN, VehicleTracking-System – sistem localizare vehicul)</p>
+            <p>– Cuplare automata pentru lumina de drum</p>
+            <p>– Cutie viteze 8 viteze – cutie viteze cu ambreiaj dublu</p>
+            <p>– (PDK)</p>
+          </div>
         </div>
       </div>
     </div>
